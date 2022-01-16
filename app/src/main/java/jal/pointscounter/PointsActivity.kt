1c750788b783a46e97206d39c29ca6d4d4b5b0ba
tschurins <thomas.schurins@gmail.com>
2022-01-16 18:51:05 +0100

    sharing the results as a picture

--- app/src/main/java/jal/pointscounter/PointsActivity.kt
+++ app/src/main/java/jal/pointscounter/PointsActivity.kt
@@ -1,7 +1,15 @@
 package jal.pointscounter
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
@@ -11,11 +19,17 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.view.children
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class PointsActivity : AppCompatActivity() {
@@ -32,7 +46,11 @@ class PointsActivity : AppCompatActivity() {
         val headerView: LinearLayout = findViewById(R.id.header_view)
         for (player in ApplicationState.players) {
             val playerTitle = TextView(this)
-            val layoutParams = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f)
+            val layoutParams = LinearLayout.LayoutParams(
+                0,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                1.0f
+            )
             layoutParams.rightMargin = 8
             layoutParams.leftMargin = 8
             playerTitle.layoutParams = layoutParams
@@ -93,10 +111,119 @@ class PointsActivity : AppCompatActivity() {
             true
         }
 
+        R.id.action_po_share -> {
+            val bitmap = createSnapshot()
+            val sdf = SimpleDateFormat("yyyyMMdd-hhmm")
+            val store = store(bitmap, "points-${sdf.format(Date())}.png")
+            if (store != null) shareImage(store)
+            else Toast.makeText(this, "No Storage Available", Toast.LENGTH_SHORT).show()
+            //sendHtml()
+            true
+        }
+
         else -> {
             // If we got here, the user's action was not recognized.
             // Invoke the superclass to handle it.
             super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun createSnapshot(): Bitmap {
+        val columns = ApplicationState.players.size
+        val rows = ApplicationState.points.points.size
+        val columnSize = 100f
+        val rowSize = 20f
+        val bitmap = Bitmap.createBitmap(
+            (columns * columnSize).toInt(),
+            ((rows + 3.25) * rowSize).toInt(),
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+
+        val paint = Paint()
+        paint.color = Color.WHITE
+        paint.style = Paint.Style.FILL
+        canvas.drawPaint(paint)
+
+        paint.color = Color.BLACK
+        paint.textSize = 16f
+        paint.isAntiAlias = true
+        val textXBase = 0.1f * columnSize
+        for (p in 0 until columns) {
+            canvas.drawText(ApplicationState.players[p], p * columnSize + textXBase, rowSize, paint)
+        }
+        canvas.drawLine(0f, 1.25f * rowSize, columns * columnSize + columnSize - 1, 1.25f * rowSize, paint)
+
+        for (r in 0 until rows) {
+            for (p in 0 until columns) {
+                canvas.drawText("${ApplicationState.points.points[r][p]}", p * columnSize + textXBase, (2.5f + r) * rowSize, paint)
+            }
+        }
+        canvas.drawLine(0f, (rows + 1.75f) * rowSize, columns * columnSize + columnSize - 1, (rows + 1.75f) * rowSize, paint)
+        for (p in 0 until columns) {
+            canvas.drawText("${ApplicationState.points.total[p]}", p * columnSize + textXBase, (rows + 3f) * rowSize, paint)
+        }
+        return bitmap
+    }
+
+    /**
+     * Stores the image to the external storage for sharing.
+     */
+    private fun store(bm: Bitmap, fileName: String): File? {
+        val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        if (dir != null) {
+            if (!dir.exists()) dir.mkdirs()
+            val file = File(dir, fileName)
+            try {
+                val fOut = FileOutputStream(file)
+                bm.compress(Bitmap.CompressFormat.PNG, 85, fOut)
+                fOut.flush()
+                fOut.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                return null
+            }
+            return file
+        } else {
+            return null
+        }
+    }
+
+    /**
+     * Open the share dialog
+     */
+    private fun shareImage(file: File) {
+        val uri = FileProvider.getUriForFile(
+            this,
+            applicationContext.packageName.toString() + ".provider",
+            file
+        )
+        val intent = Intent()
+        intent.action = Intent.ACTION_SEND
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_SUBJECT, "")
+        intent.putExtra(Intent.EXTRA_TEXT, "")
+        intent.putExtra(Intent.EXTRA_STREAM, uri)
+        val chooser = Intent.createChooser(intent, "Share Screenshot")
+        val resInfoList = this.packageManager.queryIntentActivities(
+            chooser,
+            PackageManager.MATCH_DEFAULT_ONLY
+        )
+
+        for (resolveInfo in resInfoList) {
+            val packageName = resolveInfo.activityInfo.packageName
+            grantUriPermission(
+                packageName,
+                uri,
+                Intent.FLAG_GRANT_WRITE_URI_PERMISSION or Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
+        }
+
+        chooser.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        try {
+            startActivity(chooser)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "No App Available", Toast.LENGTH_SHORT).show()
         }
     }
 }
